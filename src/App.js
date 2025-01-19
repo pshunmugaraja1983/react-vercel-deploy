bd1e17e6413f3d83a5959267a60d6ac8dfd1716d
pshunmugaraja1983 <pshunmugaraja1983@gmail.com>
2025-01-20 04:57:50 +0530

    react-app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+		<p>
+		I deployed my first app with Vercel!
+		</p>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
